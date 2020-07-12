052ecd98db3398bdd48c32061107dc961202e534
marco <serranomarco@hotmail.com>
2020-07-12 03:14:56 -0700

    making a build folder

---
+++ build/precache-manifest.9fc5bbe273d4bd91fc9651cf591e028e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0059e24b589ee66a6ca534bdd2b5119",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a3d3da1a75c4432ff48",
+    "url": "/static/css/main.5ec37ac7.chunk.css"
+  },
+  {
+    "revision": "723f422ea8726ae6a109",
+    "url": "/static/js/2.0236d585.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.0236d585.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a3d3da1a75c4432ff48",
+    "url": "/static/js/main.e5a623c8.chunk.js"
+  },
+  {
+    "revision": "dc911b369205ae8a019c",
+    "url": "/static/js/runtime-main.6c0d915a.js"
+  }
+]);
