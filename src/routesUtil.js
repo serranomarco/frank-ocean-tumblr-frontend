9122c4ab5595c0d8f37e0f81df5172255e10c25b
marco <serranomarco@hotmail.com>
2020-08-26 10:43:06 -0700

    fixing bug

--- src/routesUtil.js
+++ src/routesUtil.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
 export const PrivateRoute = ({ path, component: Component, needLogin }) => {
+    console.log('in here')
+    console.log(needLogin)
     return (
         <Route path={path} render={props => needLogin === true ? <Redirect to='/login' /> : <Component />} />
     );
